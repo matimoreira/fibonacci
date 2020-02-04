9600a236fc1fd62398fbc08fbb7b5f6c4683739a
Matias Moreira <moreiramatias10@hotmail.com>
2020-02-04 16:22:13 -0300

    Agregar archivos de proyecto.

---
+++ fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serie de Fibonacci.");
+            int valor1 = 0, valor2 = 1, cantidad, aux;
+            Console.Write("Ingresar la cantidad de numero que desea ver de la serie: ");
+            cantidad = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cantidad; i++)
+            {
+                aux = valor1;
+                valor1 = valor2;
+                valor2 = aux + valor1;
+                if (i < 3)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(valor1);
+                }
+
+
+            }
+        }
+    }
+}
